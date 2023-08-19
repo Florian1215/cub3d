1259aed1f99fe9e69832135d78acc342bf0732ea
Maxime Finette <mfinette@z4r10p7.42lyon.fr>
2023-08-19 20:40:35 +0200

    fisheye fix

--- SRCS/DRAW/fov.c
+++ SRCS/DRAW/fov.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fov.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fguirama <fguirama@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: mfinette <mfinette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 22:23:00 by fguirama          #+#    #+#             */
-/*   Updated: 2023/05/22 22:23:00 by fguirama         ###   ########lyon.fr   */
+/*   Updated: 2023/08/19 20:38:28 by mfinette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,14 @@ void	draw_fov(t_data	*data, t_ico offset)
 
 	i = 0;
 	angle = degre_to_radian(data->map->degre - (fovs[data->fov - 4] / 2));
+	printf("angle = %f\n", radian_to_degre(angle - degre_to_radian(data->map->degre)));
+	printf("map angle = %f\n", data->map->degre);
+	printf("adjusted angle = %f\n\n", angle - degre_to_radian(data->map->degre) + degre_to_radian(45));
 	while (angle <= degre_to_radian(data->map->degre + (fovs[data->fov - 4] \
 		/ 2)) && i < data->distances.size)
 	{
 		collision = draw_fov_line(data, data->map->pos, get_minimap_fov(data, \
-		angle, offset), angle - degre_to_radian(data->map->degre), offset);
+		angle, offset), angle - degre_to_radian(data->map->degre) + degre_to_radian(45), offset);
 		data->distances.distance[i] = collision.distance;
 		data->distances.face[i] = collision.side;
 		data->distances.angle[i] = angle;
@@ -48,13 +51,20 @@ static t_dco	get_minimap_fov(t_data *data, double angle, t_ico offset)
 	return (orientation);
 }
 
-double	calculate_distance(t_data *data, t_dco p1, t_dco p2)
+double	calculate_distance(t_data *data, t_dco p1, t_dco p2, double angle)
 {
 	double	distance;
-
+	
+	(void)data;
+	// angle += degre_to_radian(45);
 	distance = distance_between_points(p1, p2);
-	distance *= cos(degre_to_radian(data->map->degre - \
-		radian_to_degre(atan2(p2.y - p1.y, p2.x - p1.x))));
+	if (radian_to_degre(angle) > 45)
+		angle = angle - degre_to_radian(45);
+	else
+		angle = degre_to_radian(45) - angle;
+	distance *= cos(angle);
+ 	// distance *= cos(degre_to_radian(data->map->degre - \
+		// radian_to_degre(atan2(p2.y - p1.y, p2.x - p1.x))));
 	return (distance);
 }
 
@@ -85,7 +95,7 @@ t_collision	draw_fov_line(t_data *data, t_dco p1, t_dco p2, \
 			collision.wall = (t_dco){(p1.x), \
 				(p1.y)};
 			collision.distance = calculate_distance(data, initial, \
-				collision.wall);
+				collision.wall, angle);
 			collision.side = determine_wall_type(data, p1);
 			return (collision);
 		}
--- SRCS/raycasting.c
+++ SRCS/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: mfinette <mfinette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 16:25:27 by mfinette          #+#    #+#             */
-/*   Updated: 2023/06/10 17:31:29 by mfinette         ###   ########lyon.fr   */
+/*   Updated: 2023/08/19 19:45:12 by mfinette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ void	raycasting(t_data *data)
 	{
 		if (data->distances.distance[i])
 		{
-			edge_distance = data->distances.distance[i] * 2.5;
-			if (edge_distance + 150 < WIN_HEIGHT - edge_distance - 150)
-			{
+			edge_distance = data->distances.distance[i] * 1.5;
+			// if (edge_distance + 150 < WIN_HEIGHT - edge_distance - 150)
+			// {
 				if (data->distances.face[i] == NORTH)
 					color = 0x00FF00;
 				else if (data->distances.face[i] == EAST)
@@ -38,14 +38,14 @@ void	raycasting(t_data *data)
 					color = 0xFFFF00;
 				else if (data->distances.face[i] == FACE_ERROR)
 					color = 0xFFFFFF;
-				draw_line(data, (t_dco){i * width, 0 + edge_distance + 150}, \
-				(t_dco){i * width, WIN_HEIGHT - edge_distance - 150}, color);
-			}
-			else
-			{
-				draw_line(data, (t_dco){i * width, WIN_HEIGHT / 2 - 2}, \
-					(t_dco){i * width, WIN_HEIGHT / 2 + 2}, 0xFF0000);
-			}
+				draw_line(data, (t_dco){i * width, fmin(edge_distance, WIN_HEIGHT / 2)}, \
+				(t_dco){i * width, fmax(WIN_HEIGHT - edge_distance, WIN_HEIGHT / 2)}, color);
+			// }
+			// else
+			// {
+			// 	draw_line(data, (t_dco){i * width, WIN_HEIGHT / 2 - 2}, \
+			// 		(t_dco){i * width, WIN_HEIGHT / 2 + 2}, 0xFF0000);
+			// }
 		}
 		i++;
 	}
